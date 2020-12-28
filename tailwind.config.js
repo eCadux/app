f6e6497105c0066624d43cb6861038ee0e84023a
eCadux <noelserdna@gmail.com>
2020-12-28 20:41:27 +0100

    Instalado tailwind y git

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     // eslint-disable-next-line prettier/prettier
+     extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan
+      }
+     },
+   },
+   variants: {},
+   plugins: [],
+ }
